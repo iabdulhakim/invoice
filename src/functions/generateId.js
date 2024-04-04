f28dd3f12671fff2d6176f86b7a6c49607b44829
iabdulhakim <abdulhakimisroil@gmail.com>
2024-04-04 15:19:58 +0500

    first commit

---
+++ src/functions/generateId.js
@@ -0,0 +1,13 @@
+function generateID() {
+    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const id = "";
+    for (let i = 0; i < 2; i++) {
+      id += letters.charAt(Math.floor(Math.random() * letters.length));
+    }
+    for (let i = 0; i < 4; i++) {
+      id += Math.floor(Math.random() * 10);
+    }
+    return id;
+}
+
+export default generateID
